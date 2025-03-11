eda256d1dd67ea3e3a55f8a7bdaf7c3f46fda341
채근영 <chaulfe@gmail.com>
2025-03-11 09:49:12 +0900

    Create main.js

---
+++ learn-javascript/lectures/1_first-app/main.js
@@ -0,0 +1,31 @@
+console.log('Hello!!!');
+console.log('Hello@@', 'Hi');
+
+// alert('홍준기');
+
+let str = 'Hello Javascript';
+let number = 30;
+console.log('문자열:', str);
+console.log('number:', number);
+
+function sayHello() {
+  console.log('Hello1');
+  console.log('Hello2');
+  console.log('Hello3');
+}
+
+sayHello();
+console.log('----------');
+sayHello();
+
+let obj = {
+  number: 30, // 프로퍼티(속성)
+  sayHello: function () {
+    console.log('obj>Hello1');
+    console.log('obj>Hello2');
+    console.log('obj>Hello3');
+  },
+};
+
+console.log(obj.number);
+obj.sayHello();
