514391f87d0f2a8cdaca6fef93b22a8bb55b59db
채근영 <chaulfe@gmail.com>
2025-03-27 08:06:09 +0900

    Update main.js

--- recap-javascript/slide-images/main.js
+++ recap-javascript/slide-images/main.js
@@ -1,5 +1,5 @@
 let currentIndex = 0;
-const images = ["alt.png", "b1nd.png", "chatty.png", "cns.png", "ducami.png", "louter.png", "modi.png", "samdi.png"];
+const images = ["./image/alt.png", "./image/b1nd.png", "./image/chatty.png", "./image/cns.png", "./image/ducami.png", "./image/louter.png", "./image/modi.png", "./image/samdi.png"];
 const slideImage = document.getElementById('slideImage');
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
